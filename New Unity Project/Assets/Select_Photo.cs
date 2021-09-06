86cb939643631022adefc40ce3cba794be4e7058
장현우 <ashyonu@gmail.com>
2021-09-06 23:33:01 +0900

    Open gallery
    use plug-in

--- New Unity Project/Assets/Select_Photo.cs
+++ New Unity Project/Assets/Select_Photo.cs
@@ -10,5 +10,38 @@ public class Select_Photo : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+    public void OnClickSelect()
+    {
+        int maxSize = 512;
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            Debug.Log("Image path: " + path);
+            if (path != null)
+            {
+                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
 
+                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+                quad.transform.forward = Camera.main.transform.forward;
+                quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+
+                Material material = quad.GetComponent<Renderer>().material;
+                if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+                    material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+                material.mainTexture = texture;
+
+                Destroy(quad, 5f);
+
+                Destroy(texture, 5f);
+            }
+        });
+
+        Debug.Log("Permission result: " + permission);
+    }
 }
